f0173e13b34f107abe43967a05d0710c16fe3451
lucky <luckyjonesbuayan@gmail.com>
2025-10-30 17:40:34 +0800

    initialized modules of the nestjs server
    - user
    - transaction
    - income
    - expense
    - saving
    - liability

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { IncomeModule } from './income/income.module';
+import { ExpenseModule } from './expense/expense.module';
+import { SavingModule } from './saving/saving.module';
+import { LiabilityModule } from './liability/liability.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TransactionModule, IncomeModule, ExpenseModule, SavingModule, LiabilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
