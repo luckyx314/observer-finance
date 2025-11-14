bf5269d2ba12fafd533abb0dbb29e2981588ed5b
lucky <luckyjonesbuayan@gmail.com>
2025-11-14 14:34:22 +0800

    fix cors issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,12 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:5174', 'http://127.0.0.1:5174'],
+    origin: [
+      'http://localhost:5174',
+      'http://127.0.0.1:5174',
+      'https://finx.spoofl.com',
+      'https://www.finx.spoofl.com',
+    ],
     credentials: true,
   });
 
